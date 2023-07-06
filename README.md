cb482d6ef319de82a6632910c181e62fefa108d6
jcblsn <jacobeliason@gmail.com>
2023-07-06 11:35:13 +0100

    clean up main.py

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Great Salt Lake Water Level Bot
 
 
-The Great Salt Lake is [shrinking](https://pws.byu.edu/great-salt-lake). This repo is both a simple reminder and a proof of concept for a Bluesky bot.
+The Great Salt Lake is [shrinking](https://www.reuters.com/business/environment/utahs-great-salt-lake-is-drying-out-threatening-ecological-economic-disaster-2022-07-14/). This repo is a simple reminder and a proof of concept for a Bluesky bot.
+
+## Functionality
+
+I scrape the current water level data from the [Great Salt Lake Water Level](http://greatsalt.uslakes.info/Level.asp) website, log it to `levels.csv`, and post it to Bluesky using the [atprototools](https://github.com/iandklatzco/atprototools) package.
 
 ## Requirements
 
--- main.py
+++ main.py
@@ -1,75 +1,64 @@
-from atprototools import Session
-from bs4 import BeautifulSoup
-import requests, csv, os
-from datetime import datetime, timedelta
-import re
-
-
+import os
 import json
+import csv
+import re
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from atprototools import Session
+
 with open('credentials.json') as f:
     creds = json.load(f)
-USERNAME = creds["BSKY_USERNAME"]
-PASSWORD = creds["BSKY_PASSWORD"]
-session = Session(USERNAME, PASSWORD)
 
-session = Session(os.environ.get('BSKY_USERNAME'), os.environ.get('BSKY_PASSWORD'))
+BSKY_USERNAME = os.environ.get('BSKY_USERNAME') or creds.get("BSKY_USERNAME")
+BSKY_PASSWORD = os.environ.get('BSKY_PASSWORD') or creds.get("BSKY_PASSWORD")
+URL = "http://greatsalt.uslakes.info/Level.asp"
+FILENAME = "levels.csv"
+HEADERS = ['dt', 'water_level', 'timestamp_utc']
 
-url = "http://greatsalt.uslakes.info/Level.asp"
-response = requests.get(url)
+def scrape_water_level_info(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    water_level_info = soup.find('div', style=lambda value: 'font-size:46px' in value if value else False)
+    water_level = float(water_level_info.text.strip().replace(",", ""))
+    font_elements = soup.find_all('font')
+    date_info = next((el for el in font_elements if re.search(r"\b(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), \b", el.text)), None)
+    time_info = next((el for el in font_elements if re.search(r"\b(?:[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] [AP]M\b", el.text)), None)
+    date = datetime.strptime(date_info.text.strip(), "%A, %B %d, %Y") if date_info is not None else None
+    time = datetime.strptime(time_info.text.strip(), "%I:%M:%S %p") if time_info is not None else None
+    dt = datetime.combine(date, time.time())
+    return water_level, dt
 
-soup = BeautifulSoup(response.text, 'html.parser')
-
-water_level_info = soup.find('div', style=lambda value: 'font-size:46px' in value if value else False)
-water_level = float(water_level_info.text.strip().replace(",", ""))
-
-font_elements = soup.find_all('font')
-date_info = next((el for el in font_elements if re.search(r"\b(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), \b", el.text)), None)
-time_info = next((el for el in font_elements if re.search(r"\b(?:[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] [AP]M\b", el.text)), None)
-if date_info is not None:
-    date = datetime.strptime(date_info.text.strip(), "%A, %B %d, %Y")
-if time_info is not None:
-    time = datetime.strptime(time_info.text.strip(), "%I:%M:%S %p")
-date = datetime.strptime(date_info.text.strip(), "%A, %B %d, %Y")
-time = datetime.strptime(time_info.text.strip(), "%I:%M:%S %p")
-dt = datetime.combine(date, time.time())
-
-filename = "levels.csv"
-headers = ['dt', 'water_level', 'timestamp']
-
-if not os.path.exists(filename) or os.stat(filename).st_size == 0:
-    with open(filename, 'w', newline='') as file:
+def update_csv(filename, headers, dt, water_level):
+    if not os.path.exists(filename) or os.stat(filename).st_size == 0:
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+        data = data[1:]
+    timestamp_utc = datetime.utcnow().strftime("%Y-%m-%d_%H%M%S")
+    data.append([dt.strftime("%Y-%m-%d_%H%M%S"), water_level, timestamp_utc])
+    
+    with open(filename, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(headers)
+        writer.writerows(data)
 
-with open(filename, 'r') as file:
-    reader = csv.reader(file)
-    data = list(reader)
-    data = data[1:]
+def main():
+    session = Session(BSKY_USERNAME, BSKY_PASSWORD)
+    water_level, dt = scrape_water_level_info(URL)
+    update_csv(FILENAME, HEADERS, dt, water_level)
+    caption = f"Current water level of the Great Salt Lake: {water_level} feet MSL on {dt.strftime('%A, %B %d, %Y')} at {dt.strftime('%I:%M:%S %p')} MT."
+    caption += "\n"
+    # caption += "Source: http://greatsalt.uslakes.info/Level.asp"
+    caption += "\n"
+    caption += "A reminder: the Great Salt Lake is disappearing. Please consider getting involved to help save the lake: www.fogsl.org"
+    resp = session.postBloot(caption)
+    if resp.status_code == 200:
+        print("Post created successfully!")
+    else:
+        print(f"Failed to create post: {resp.content}")
 
-day_change = week_change = month_change = "N/A"
-for row in data:
-    row_date = datetime.strptime(row[0], "%Y-%m-%d_%H%M%S")
-    if row_date == date - timedelta(days=1):
-        day_change = water_level - float(row[1])
-    # elif row_date == date - timedelta(days=7):
-    #     week_change = water_level - float(row[1])
-    # elif row_date == date - timedelta(days=30):
-    #     month_change = water_level - float(row[1])
-
-timestamp_utc = datetime.utcnow().strftime("%Y-%m-%d_%H%M%S")
-data.append([dt.strftime("%Y-%m-%d_%H%M%S"), water_level, timestamp_utc])
-with open(filename, "a", newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(data)
-
-caption = f"Current water level of the Great Salt Lake: {water_level} feet MSL on {date.strftime('%A, %B %d, %Y')} at {time.strftime('%I:%M:%S %p')} MT."
-# caption += f"\n\nChange from yesterday: {day_change} feet.\n\nChange from last week: {week_change} feet.\n\nChange from last month: {month_change} feet."
-# caption += f"\n\nSource: http://greatsalt.uslakes.info/Level.asp"
-caption += f"\n\nA reminder: the Great Salt Lake is disappearing. Please consider signing the petition to help save the lake: www.saveourgreatsaltlake.org"
-
-resp = session.postBloot(caption)
-
-if resp.status_code == 200:
-    print("Post created successfully!")
-else:
-    print(f"Failed to create post: {resp.content}")
+if __name__ == '__main__':
+    main()
